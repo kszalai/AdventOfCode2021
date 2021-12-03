718e65fc3c63e8a2804c53194b01db7f2faae370
Kyle Szalai <kyleszalai@gmail.com>
2021-12-03 12:28:51 -0500

    Restructing day 1 puzzles

--- Day1/Puzzle2.kt
+++ Day1/Puzzle2.kt
@@ -3,16 +3,29 @@ import java.io.File
 fun main() {
     val lineList = ArrayList<Int>()
 
-    File("../input.txt").useLines { lines -> 
-        lines.forEach { 
+    File("input.txt").useLines { lines ->
+        lines.forEach {
             lineList.add(it.toInt())
         }
+    }
+
+    val windows = ArrayList<Int>()
+
+    var value1: Int
+    var value2: Int
+    var value3: Int
+
+    for (i in 2..lineList.size-1) {
+        value1 = lineList[i - 2]
+        value2 = lineList[i - 1]
+        value3 = lineList[i]
+        windows.add(value1 + value2 + value3)
     }
 
     var amountOfIncreases = 0
     var previousValue = -1
 
-    lineList.forEach {
+    windows.forEach {
         if (previousValue == -1) {
             previousValue = it
             return@forEach
