bbed595a0ebfbb2450d96a2538e2e679c999e6fb
Kyle Szalai <kyleszalai@gmail.com>
2021-12-24 11:06:51 -0500

    Solving puzzle 1 for day 11
    Adding common function to create an int matrix

--- src/main/kotlin/common/common.kt
+++ src/main/kotlin/common/common.kt
@@ -1,6 +1,7 @@
 package common
 
 import java.io.File
+import java.lang.Integer.parseInt
 
 fun <T> readInputFile(path: String): ArrayList<T> {
     val lineList = ArrayList<T>()
@@ -16,4 +17,18 @@ fun <T> readInputFile(path: String): ArrayList<T> {
 
 fun readIntCSVInputFile(path: String): ArrayList<Int> {
     return File(path).bufferedReader().use { reader -> reader.readLine() }.split(",").map { number -> number.toInt() } as ArrayList<Int>
+}
+
+fun readIntMatrix(path: String): List<List<Int>> {
+    val matrix = mutableListOf<List<Int>>()
+
+    File(path).useLines { lines ->
+        lines.forEach {
+            val row = mutableListOf<Int>()
+            it.map { digit -> row.add(parseInt(digit.toString())) }
+            matrix.add(row)
+        }
+    }
+
+    return matrix
 }
