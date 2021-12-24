37996a7513376f2a26bc1326713c06f4ef5557b5
Kyle Szalai <kyleszalai@gmail.com>
2021-12-24 11:20:49 -0500

    Refactoring day 11 puzzle 1, so I can declare objects and properties for puzzle 2

--- src/main/kotlin/day11/Puzzle1.kt
+++ src/main/kotlin/day11/Puzzle1.kt
@@ -2,82 +2,87 @@ package day11
 
 import common.readIntMatrix
 
-var flashes = 0
-
 fun main() {
     var input = readIntMatrix("src/main/kotlin/day11/input.txt")
 
     var steps = 100
 
     while (steps != 0) {
-        input = input.map(::increaseRowBy1)
-        input = checkNearbySquids(input, input.map { row -> row.map { false }.toMutableList() }.toMutableList())
+        input = input.map(Day11Puzzle1::increaseRowBy1)
+        input = Day11Puzzle1.checkNearbySquids(
+            input,
+            input.map { row -> row.map { false }.toMutableList() }.toMutableList()
+        )
         steps--
     }
 
-    println(flashes)
+    println(Day11Puzzle1.flashes)
 }
 
-fun increaseRowBy1(row: List<Int>) = row.map(::increaseBy1)
+object Day11Puzzle1 {
+    var flashes = 0
 
-fun increaseBy1(cell: Int) = cell + 1
+    fun increaseRowBy1(row: List<Int>) = row.map(::increaseBy1)
 
-fun checkNearbySquids(matrix: List<List<Int>>, visited: MutableList<MutableList<Boolean>>): List<List<Int>> {
-    var modified = matrix.map { row ->
-        row.toMutableList()
-    }.toMutableList()
+    fun increaseBy1(cell: Int) = cell + 1
 
-    val pointsAbove9 = modified.mapIndexed { i, row ->
-        row.mapIndexed { j, cell ->
-            if (cell > 9 && !visited[i][j]) Pair(i, j) else null
-        }.filterNotNull()
-    }.flatten().toMutableList()
+    fun checkNearbySquids(matrix: List<List<Int>>, visited: MutableList<MutableList<Boolean>>): List<List<Int>> {
+        var modified = matrix.map { row ->
+            row.toMutableList()
+        }.toMutableList()
 
-    if (pointsAbove9.isNotEmpty()) {
-        pointsAbove9.map {
-            visited[it.first][it.second] = true
-            // Check up
-            if (it.first - 1 >= 0) {
-                modified[it.first - 1][it.second]++
+        val pointsAbove9 = modified.mapIndexed { i, row ->
+            row.mapIndexed { j, cell ->
+                if (cell > 9 && !visited[i][j]) Pair(i, j) else null
+            }.filterNotNull()
+        }.flatten().toMutableList()
+
+        if (pointsAbove9.isNotEmpty()) {
+            pointsAbove9.map {
+                visited[it.first][it.second] = true
+                // Check up
+                if (it.first - 1 >= 0) {
+                    modified[it.first - 1][it.second]++
+                }
+                // Check down
+                if (it.first + 1 < matrix.size) {
+                    modified[it.first + 1][it.second]++
+                }
+                // Check left
+                if (it.second - 1 >= 0) {
+                    modified[it.first][it.second - 1]++
+                }
+                // Check right
+                if (it.second + 1 < matrix[it.first].size) {
+                    modified[it.first][it.second + 1]++
+                }
+                // Check up && left
+                if (it.first - 1 >= 0 && it.second - 1 >= 0) {
+                    modified[it.first - 1][it.second - 1]++
+                }
+                // Check up && right
+                if (it.first - 1 >= 0 && it.second + 1 < matrix[it.first - 1].size) {
+                    modified[it.first - 1][it.second + 1]++
+                }
+                // Check down && left
+                if (it.first + 1 < matrix.size && it.second - 1 >= 0) {
+                    modified[it.first + 1][it.second - 1]++
+                }
+                // Check down && right
+                if (it.first + 1 < matrix.size && it.second + 1 < matrix[it.first + 1].size) {
+                    modified[it.first + 1][it.second + 1]++
+                }
             }
-            // Check down
-            if (it.first + 1 < matrix.size) {
-                modified[it.first + 1][it.second]++
-            }
-            // Check left
-            if (it.second - 1 >= 0) {
-                modified[it.first][it.second - 1]++
-            }
-            // Check right
-            if (it.second + 1 < matrix[it.first].size) {
-                modified[it.first][it.second + 1]++
-            }
-            // Check up && left
-            if (it.first - 1 >= 0 && it.second - 1 >= 0) {
-                modified[it.first - 1][it.second - 1]++
-            }
-            // Check up && right
-            if (it.first - 1 >= 0 && it.second + 1 < matrix[it.first - 1].size) {
-                modified[it.first - 1][it.second + 1]++
-            }
-            // Check down && left
-            if (it.first + 1 < matrix.size && it.second - 1 >= 0) {
-                modified[it.first + 1][it.second - 1]++
-            }
-            // Check down && right
-            if (it.first + 1 < matrix.size && it.second + 1 < matrix[it.first + 1].size) {
-                modified[it.first + 1][it.second + 1]++
-            }
+            modified = checkNearbySquids(modified, visited).map { row -> row.toMutableList() }.toMutableList()
         }
-        modified = checkNearbySquids(modified, visited).map { row -> row.toMutableList() }.toMutableList()
-    }
 
-    return modified.map { row ->
-        row.map { cell ->
-            if (cell > 9) {
-                flashes++
-                0
-            } else cell
+        return modified.map { row ->
+            row.map { cell ->
+                if (cell > 9) {
+                    flashes++
+                    0
+                } else cell
+            }
         }
     }
 }
